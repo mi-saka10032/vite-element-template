fa425bfe767f256dff4d138352abb8e03a3e0951
Misaka10032 <misaka10032@aliyun.com>
2022-05-17 09:43:44 +0800

    修改Mock插入逻辑

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,12 @@ import { defineConfig } from 'vite'
 import { createVuePlugin } from 'vite-plugin-vue2'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
-import { viteMockServe } from 'vite-plugin-mock'
 
 export default ({ mode }) => {
   return defineConfig({
     plugins: [
       createVuePlugin(),
       vueJsx(),
-      viteMockServe(),
       createSvgIconsPlugin({
         iconDirs: [resolve(process.cwd(), 'src/assets/icons/svg')],
         symbolId: 'icon-[dir]-[name]'
